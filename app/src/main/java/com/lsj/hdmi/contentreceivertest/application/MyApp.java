f79e49ed262018eb627698f3105fa2c2cd5c6e15
q310208626 <15521385907@163.com>
2017-06-05 16:40:58 +0800

    去掉了数据库daoConfig

--- app/src/main/java/com/lsj/hdmi/contentreceivertest/application/MyApp.java
+++ app/src/main/java/com/lsj/hdmi/contentreceivertest/application/MyApp.java
@@ -12,21 +12,11 @@ import java.io.File;
  * Created by hdmi on 17-6-3.
  */
 public class MyApp extends Application {
-    //本地数据库
-    private String localDBName="myMusicDB.db";
-    private File localDBDir=new File(Environment.getExternalStorageDirectory().getPath());
-    private int localDBVersion=1;
-    public static DbManager.DaoConfig daoConfig;
+
     @Override
     public void onCreate() {
         super.onCreate();
         x.Ext.init(this);
         x.Ext.setDebug(true);
-
-        daoConfig=new DbManager.DaoConfig()
-                .setDbName(localDBName)
-                .setDbDir(localDBDir)
-                .setDbVersion(localDBVersion)
-                .setAllowTransaction(true);
     }
 }
